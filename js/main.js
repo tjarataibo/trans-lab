1f58bb82058e3cff1122818d958671228d35da84
Tamara <tjarataibo@gmail.com>
2018-06-28 11:00:05 -0400

    fetch visible con consolelog, pero sin poder llamar a la función

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
-/* Holiii acá va tu código también */
+// saldo = () => 
+// {
+//     document.getElementById("").innerHTML
+// }
+
+// tarifa = () => 
+// {
+//     document.getElementById("").innerHTML
+// }
+
+// perfil = () => 
+// {
+//     document.getElementById("").innerHTML
+// }
+
+// preguntas = () => 
+// {
+//     document.getElementById("#navbarToggleExternalContentTwo").innerHTML
+// }
+
+// home = () => 
+// {
+//     document.getElementById("").innerHTML
+// }
+
+
+//con esto tomo el numero de codigo que ingrese el usuario
+let input = document.querySelector('input');
+
+function getBip(num){
+
+        // 13 is enter
+          // code for enter
+          input.addEventListener("keypress",(enter)=>{
+            let key = enter.which || enter.keyCode;
+            if (key === 13) {
+            
+
+
+          fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${num}`) 
+            .then(response => response.json())
+            .then(data => {
+              
+                console.log(data);
+                
+             })
+            }
+    
+        })
+    }
+     console.log(getBip('77108068'))
